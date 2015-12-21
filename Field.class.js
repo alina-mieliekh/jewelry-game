a0a8dd08550ebd0027fc50350e9bca3bbf057748
Alina Mieliekh <alina.mieliekh@gmail.com>
2015-12-21 23:43:21 +0100

    finished render function

--- Field.class.js
+++ Field.class.js
@@ -7,12 +7,40 @@ function Field (gameContainerId, fieldHeight, fieldWidth){
     var initialize = function(){
         gameContainer = document.getElementById(gameContainerId);
         generateField();
-    };
+        this.render();
+    }.bind(this);
 
     this.render = function () {
+        var html = '';
+        var map = {
+            1:'rhombus',
+            2:'circle',
+            3:'star',
+            4:'pentagon',
+            5:'heart'
+        };
+
+        for (i=0; i<field.length; i++){
+            html += '<div class="row">';
+            for (j=0; j<field[i].length; j++){
+                //console.log(field[i][j]);
+                html +=
+                    '<div class="cell">' +
+                        '<div class="type-'+ map[field[i][j]] + '" >' +
+                        '</div>' +
+                    '</div>';
+            }
+            html += '</div>';
+        }
+        gameContainer.innerHTML=html;
+        console.log(html);
+
 
     };
 
+    var getRandomInt = function(min, max) {
+        return Math.floor(Math.random() * (max + 1 - min)) + min;
+    };
 
     var generateField = function(){
 
@@ -23,7 +51,7 @@ function Field (gameContainerId, fieldHeight, fieldWidth){
             tempArray = [];
 
             for (var j=0; j<fieldWidth; j++){
-                tempArray.push(null);
+                tempArray.push( getRandomInt(1, 5) );
             }
             field.push(tempArray);
         }
@@ -35,7 +63,7 @@ function Field (gameContainerId, fieldHeight, fieldWidth){
 
 }
 
-var myField = new Field('gameContainer', 10, 10);
+var myField = new Field('gameContainer', 10, 20);
 myField.render();
 
 
